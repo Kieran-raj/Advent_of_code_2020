75dbe64b15542805b8503e07edc6380ecb01c503
Kieran Rajasansir <kieran@rajasansir.co.uk>
2020-12-22 15:29:28 +0000

    Updates

--- day6/day6.py
+++ day6/day6.py
@@ -1,4 +1,4 @@
-with open('test.txt', 'r') as f:
+with open('input.txt', 'r') as f:
     lines = [line.replace(r'\n', ' ') for line in f.readlines()]
     lines = [line.strip() for line in lines]
     
@@ -17,26 +17,29 @@ def part_one(lines):
 
 
 def part_two(lines):
+    lines = ('_'.join([line if len(line) == 0 else line for line in lines])).split('__')
+    use = [[i] for i in lines]
     count = 0
-    dic = {}
-    lst_dics = []
-    for i in lines:
-        length_group = 0
-        if i == '':
-            dic = {}            
-        for char in i:
-            length_group += 1
-            if char not in dic.keys():
-                dic[char] = 1
-            else:
-                dic[char] += 1
-        print('length of group',length_group)
-        print(dic)
-        print('length of dic', len(dic))
-         
+    for i in use:
+        num_groups = 1
+        for j in i:
+            num_groups += j.count('_')
+            dic = {}
+            for char in j:
+                if char not in dic.keys():
+                    if char != '_':
+                        dic[char] = 0
+                        dic[char] += 1 
+                else:
+                    dic[char] += 1
+            for letter in dic:
+                if dic[letter] == num_groups:
+                    count += 1
+    print(count)                        
+            
+             
 
 
 #print(part_one(lines))
 part_two(lines)
-print(lines)
 
