bcc2bd56bc854e67638ed87d804d4b1397fde6da
Kieran Rajasansir <kieran@rajasansir.co.uk>
2020-12-23 18:21:52 +0000

    started again

--- day7/day7.py
+++ day7/day7.py
@@ -1,29 +1,4 @@
 with open('input.txt', 'r') as f:
     lines = f.readlines()
-<<<<<<< HEAD
-=======
 
 
-def search_function(lst, line):
-    index = line[0].find(' bags')
-    bags = []
-    for a in lst:
-        if line[0][:index] in a[0] and a[0][:10] != line[0][:index]:
-            bags.append(line[0][:index])
-    print(set(bags))
-
-
-def get_shiny_list(lines):
-    list_of_lines = [[line.strip()] for line in lines]
-    lines_with_shiny = []
-    count = 0
-    for a in list_of_lines:
-        if 'shiny gold' in a[0] and a[0][:10] != 'shiny gold':
-            count += 1
-            lines_with_shiny.append(a)
-
-    for i in lines_with_shiny:
-        search_function(list_of_lines, i)
-
-get_shiny_list(lines)
->>>>>>> 998cb00aa158c0439a08ee608a7fac5ff02794b1
