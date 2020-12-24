ba17c07a5ddb128028cef173a52b58f7801e9169
Kieran Rajasansir <kieran@rajasansir.co.uk>
2020-12-24 17:59:12 +0000

    Started part 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,7 +1,3 @@
-with open('input.txt', 'r') as f:
-    moves = [i.strip() for i in f.readlines()]
-
-
 def list_moves(moves):
     moves = [i.split() for i in moves]
     for i in (moves):
@@ -10,10 +6,11 @@ def list_moves(moves):
         i[1] = sign
         i.append(value)
         i.append(0)
-    return calc(moves)
+    calc_part_two(moves)
+    # return calc_part_one(moves)
 
 
-def calc(moves):
+def calc_part_one(moves):
     count = 0
     idx = 0
     while moves[idx][3] == 0:
@@ -27,8 +24,19 @@ def calc(moves):
         elif moves[idx][0] == 'jmp':
             moves[idx][3] = 1
             idx = eval(f'idx {moves[idx][1]} {moves[idx][2]}')
-
     return count
-        
 
-print(list_moves(moves))
+
+def calc_part_two(moves):
+    pass
+
+
+if __name__ == "__main__":
+    with open('test.txt', 'r') as f:
+        moves = [i.strip() for i in f.readlines()]
+    
+    print(list_moves(moves))
+
+
+
+
